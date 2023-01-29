adafc33e03ff8b51372c2e1e8a95936595921eae
iza lumpio <iza.lumpio@edu.sait.ca>
2023-01-29 14:22:07 -0700

    variable exercises

---
+++ variables.js
@@ -0,0 +1,17 @@
+// variable
+var myName = "Iza";
+alert(myName);
+
+function test(){
+    var a = "3";
+    var b = "8";
+
+    var c = a;
+    a = b;
+    b = c;
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
+test()
