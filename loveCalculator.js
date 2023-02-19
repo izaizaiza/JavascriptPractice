54b11f003c7c771e9ef347b714e87f3b7ec01daf
iza lumpio <iza.lumpio@edu.sait.ca>
2023-02-19 16:37:31 -0700

    done

--- loveCalculator.js
+++ loveCalculator.js
@@ -1,4 +1,12 @@
 // a way of generating a random number
+let yourName = prompt("What is your name: ");
+let theirName = prompt("What is their name: ");
+var loveScore = Math.random()*100;
+loveScore = Math.floor(loveScore) + 1;
 
-var n = Math.random();
-console.log(n);
+// control flow using conditionals
+if(loveScore > 75){
+    alert("Your love score is " + loveScore + "%. Your love is like how Kanye loves Kanye.");
+}else{
+    alert("Your love score is " + loveScore + "%.");
+}
