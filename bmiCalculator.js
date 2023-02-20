a9f10e1a4fff37571829a47948d4a7006cf7bee0
iza lumpio <iza.lumpio@edu.sait.ca>
2023-02-19 18:36:04 -0700

    more edits

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -1,10 +1,20 @@
 // create a BMI calculator using JavaScript Functions
 // formula = bmi = weight (kg) / height squared (meters squared)
 
+
 function bmiCalculator(weight,height){
-    var bmi = weight / (height**2);
-    return Math.round(bmi);
+    var bmi = Math.round(weight / (height**2));
+    let interpretation="";
+    if( bmi < 18.5){
+        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+    }else if (bmi>= 18.5 && bmi<= 24.9){
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+    }else{
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+    }
+    return interpretation;
 }
 
-var bmi = bmiCalculator(65,1.8);
-console.log(bmi);
+let yourWeight = prompt("Enter your weight (kg): ");
+let yourHeight = prompt("Enter your height (m): ");
+alert(bmiCalculator(yourWeight,yourHeight));
