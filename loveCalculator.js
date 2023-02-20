e7251358639bc81a236e31db43ba5450ee85d822
iza lumpio <iza.lumpio@edu.sait.ca>
2023-02-19 18:21:59 -0700

    added control flow

--- loveCalculator.js
+++ loveCalculator.js
@@ -5,8 +5,10 @@ var loveScore = Math.random()*100;
 loveScore = Math.floor(loveScore) + 1;
 
 // control flow using conditionals
-if(loveScore > 75){
+if( loveScore > 30 && loveScore>=70){
     alert("Your love score is " + loveScore + "%. Your love is like how Kanye loves Kanye.");
+}else if (loceScore <=30){
+    alert("Your love score is " + loveScore + "%." + " It's like oild and water...");
 }else{
     alert("Your love score is " + loveScore + "%.");
 }
